7fc35747021a26af3d55d568afbbbbe85cbc9288
Jonathan Chao <jonathan.chao@mimik.com>
2019-09-09 10:29:12 -0700

    AAND-992 Change null to undefined

--- src/models/queue.js
+++ src/models/queue.js
@@ -36,7 +36,7 @@ function makeQueueModel(context) {
               || data.readStatus === 'unread')
               && data.readStatus)
               || 'unread',
-      deletableTime: (useDeletableTime && data.deletableTime) || null,
+      deletableTime: (useDeletableTime && data.deletableTime) || undefined,
     };
     storage.setItem(item.id, JSON.stringify(item));
     return item;
