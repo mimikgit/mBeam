1fb84d6064e25c5dd2eb9452711a8916d9d541a1
Jeremy Hsu <jeremy.hsu@mimik.com>
2019-11-28 15:30:46 -0800

    EDGEMS-269 refactored

--- src/controllers/queueController.js
+++ src/controllers/queueController.js
@@ -1,13 +1,13 @@
 const response = require('edge-ms-helper/response-helper');
 const makeQueueProcessor = require('../processors/queueProcessor');
-const { ParameterError } = require('../models/errors');
 
 function createItem(req, res) {
   const { context } = req;
   const { ownerCode, item } = req.swagger.params;
   const queueProcessor = makeQueueProcessor(req.context);
 
-  queueProcessor.createItem(item, !!ownerCode && ownerCode === context.env.ownerCode)
+  const allowDelete = !!ownerCode && ownerCode === context.env.ownerCode;
+  queueProcessor.createItem(item, allowDelete)
     .next((createdItem => response.sendResult(createdItem, 200, res)))
     .guard(err => response.sendHttpError(err, res))
     .go();
@@ -15,62 +15,45 @@ function createItem(req, res) {
 
 function getItemList(req, res) {
   const { context } = req;
-  const { ownerCode } = req.swagger.params;
   const queueProcessor = makeQueueProcessor(context);
 
-  if (ownerCode !== context.env.ownerCode) {
-    response.sendHttpError(new ParameterError('incorrect ownerCode'), res);
-  } else {
-    queueProcessor.getItemList()
-      .next((data => response.sendResult({ data }, 200, res)))
-      .guard(err => response.sendHttpError(err, res))
-      .go();
-  }
+  queueProcessor.getItemList()
+    .next((data => response.sendResult({ data }, 200, res)))
+    .guard(err => response.sendHttpError(err, res))
+    .go();
 }
 
 function getItem(req, res) {
   const { context } = req;
-  const { ownerCode, id } = req.swagger.params;
+  const { id } = req.swagger.params;
   const queueProcessor = makeQueueProcessor(context);
 
-  if (ownerCode !== context.env.ownerCode) {
-    response.sendHttpError(new ParameterError('incorrect ownerCode'), res);
-  } else {
-    queueProcessor.getItem(id)
-      .next((item => response.sendResult(item, 200, res)))
-      .guard(err => response.sendHttpError(err, res))
-      .go();
-  }
+  queueProcessor.getItem(id)
+    .next((item => response.sendResult(item, 200, res)))
+    .guard(err => response.sendHttpError(err, res))
+    .go();
 }
 
 function deleteItem(req, res) {
   const { context } = req;
-  const { ownerCode, id } = req.swagger.params;
+  const { id } = req.swagger.params;
   const queueProcessor = makeQueueProcessor(context);
 
-  if (ownerCode !== context.env.ownerCode) {
-    response.sendHttpError(new ParameterError('incorrect ownerCode'), res);
-  } else {
-    queueProcessor.deleteItem(id)
-      .next((item => response.sendResult(item, 200, res)))
-      .guard(err => response.sendHttpError(err, res))
-      .go();
-  }
+  queueProcessor.deleteItem(id)
+    .next((item => response.sendResult(item, 200, res)))
+    .guard(err => response.sendHttpError(err, res))
+    .go();
 }
 
 function setItemReadStatus(req, res) {
   const { context } = req;
-  const { ownerCode, id, queueUpdate } = req.swagger.params;
+  const { id, queueUpdate } = req.swagger.params;
   const queueProcessor = makeQueueProcessor(context);
 
-  if (ownerCode !== context.env.ownerCode) {
-    response.sendHttpError(new ParameterError('incorrect ownerCode'), res);
-  } else {
-    queueProcessor.setReadStatus(id, queueUpdate)
-      .next((item => response.sendResult(item, 200, res)))
-      .guard(err => response.sendHttpError(err, res))
-      .go();
-  }
+  queueProcessor.setReadStatus(id, queueUpdate)
+    .next((item => response.sendResult(item, 200, res)))
+    .guard(err => response.sendHttpError(err, res))
+    .go();
 }
 
 module.exports = {
--- src/controllers/tokenController.js
+++ src/controllers/tokenController.js
@@ -1,15 +1,12 @@
 const response = require('edge-ms-helper/response-helper');
 const makeTokenProcessor = require('../processors/tokenProcessor');
-const { ParameterError } = require('../models/errors');
 
 function createToken(req, res) {
   const { context, swagger } = req;
-  const { ownerCode, tokenData } = swagger.params;
+  const { tokenData } = swagger.params;
   const tokenProcessor = makeTokenProcessor(context);
 
-  if (ownerCode !== context.env.ownerCode) {
-    response.sendHttpError(new ParameterError('incorrect ownerCode'), res);
-  } else if (!context.env.signatureKey) {
+  if (!context.env.signatureKey) {
     response.sendHttpError(new Error('edge container is missing "signatureKey"'), res);
   } else {
     tokenProcessor.createToken(tokenData)
@@ -20,63 +17,46 @@ function createToken(req, res) {
 }
 
 function getTokens(req, res) {
-  const { context, swagger } = req;
-  const { ownerCode } = swagger.params;
+  const { context } = req;
   const tokenProcessor = makeTokenProcessor(context);
 
-  if (ownerCode !== context.env.ownerCode) {
-    response.sendHttpError(new ParameterError('incorrect ownerCode'), res);
-  } else {
-    tokenProcessor.getTokens()
-      .next(data => response.sendResult({ data }, 200, res))
-      .guard(err => response.sendHttpError(err, res))
-      .go();
-  }
+  tokenProcessor.getTokens()
+    .next(data => response.sendResult({ data }, 200, res))
+    .guard(err => response.sendHttpError(err, res))
+    .go();
 }
 
 function getTokenById(req, res) {
   const { context, swagger } = req;
-  const { id, ownerCode } = swagger.params;
+  const { id } = swagger.params;
   const tokenProcessor = makeTokenProcessor(context);
 
-  if (ownerCode !== context.env.ownerCode) {
-    response.sendHttpError(new ParameterError('incorrect ownerCode'), res);
-  } else {
-    tokenProcessor.getToken(id)
-      .next(data => response.sendResult({ data }, 200, res))
-      .guard(err => response.sendHttpError(err, res))
-      .go();
-  }
+  tokenProcessor.getToken(id)
+    .next(data => response.sendResult({ data }, 200, res))
+    .guard(err => response.sendHttpError(err, res))
+    .go();
 }
 
 function updateToken(req, res) {
   const { context, swagger } = req;
-  const { id, ownerCode, tokenData } = swagger.params;
+  const { id, tokenData } = swagger.params;
   const tokenProcessor = makeTokenProcessor(context);
 
-  if (ownerCode !== context.env.ownerCode) {
-    response.sendHttpError(new ParameterError('incorrect ownerCode'), res);
-  } else {
-    tokenProcessor.updateToken(id, tokenData)
-      .next(data => response.sendResult({ data }, 200, res))
-      .guard(err => response.sendHttpError(err, res))
-      .go();
-  }
+  tokenProcessor.updateToken(id, tokenData)
+    .next(data => response.sendResult({ data }, 200, res))
+    .guard(err => response.sendHttpError(err, res))
+    .go();
 }
 
 function deleteToken(req, res) {
   const { context, swagger } = req;
-  const { id, ownerCode } = swagger.params;
+  const { id } = swagger.params;
   const tokenProcessor = makeTokenProcessor(context);
 
-  if (ownerCode !== context.env.ownerCode) {
-    response.sendHttpError(new ParameterError('incorrect ownerCode'), res);
-  } else {
-    tokenProcessor.deleteToken(id)
-      .next(data => response.sendResult({ data }, 200, res))
-      .guard(err => response.sendHttpError(err, res))
-      .go();
-  }
+  tokenProcessor.deleteToken(id)
+    .next(data => response.sendResult({ data }, 200, res))
+    .guard(err => response.sendHttpError(err, res))
+    .go();
 }
 
 module.exports = {
